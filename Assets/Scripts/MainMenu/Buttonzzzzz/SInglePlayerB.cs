2ad6274e0431de7b997a7981d3a484816bf7d87e
Thyne <Thyneschool@gmail.com>
2026-06-04 16:18:23 +0200

    SceneOvergang toegevoegd

---
+++ Assets/Scripts/MainMenu/Buttonzzzzz/SInglePlayerB.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SInglePlayerB : MonoBehaviour
+{
+    public void SinglePlayerOnClick()
+    {
+        SceneManager.LoadScene("SinglePlayerScene");
+    }
+
+    public void BlackJackOnCLick()
+    {
+        SceneManager.LoadScene("BlackJackScene");
+    }
+}
+
+
--- Assets/Scripts/Minigames/Blackjack/DeckManager.cs
+++ Assets/Scripts/Minigames/Blackjack/DeckManager.cs
@@ -117,4 +117,6 @@ public class DeckManager : MonoBehaviour
     {
         DrawAndSpawnCard(cardSpawnPoint.position);
     }
+
+    
 }
