b843158c36823021a20929f35d061e72b1928c7b
Thyne <Thyneschool@gmail.com>
2026-06-05 11:25:32 +0200

    OnlineLeaderBoardStarted & Buttons added from return to zero

---
+++ Assets/Scripts/Minigames/ReturnScene.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnScene : MonoBehaviour
+{
+    public void SinglePlayerOnClick()
+    {
+        SceneManager.LoadScene("SinglePlayerScene");
+    }
+
+    public void MainMenuOnClick()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
